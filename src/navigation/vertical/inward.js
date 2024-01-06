a9d6bebc00b1f81fedb180c1e242aec4bb1abd03
Uma_nagaraj <triosdev@redpepperdigital.net>
2024-01-06 09:21:33 +0530

    design changes

--- src/navigation/vertical/inward.js
+++ src/navigation/vertical/inward.js
@@ -4,7 +4,7 @@ import { FileText } from 'react-feather'
 export default [
   {
     id: 'inward',
-    title: 'Inward  ',
+    title: 'Stock Inward  ',
     icon: <FileText size={20} />,
     navLink: 'transaction/inward'
   }
